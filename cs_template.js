4252dc6e6b9427ea05f029b9d13bad71c03b2de1
Rodrigo Osvaldo Dominguez <rdominguez@kaloni.com>
2020-09-27 00:52:54 -0500

    Se crean las plantillas para UserEvent y ClientScript

---
+++ cs_template.js
@@ -0,0 +1,57 @@
+/**
+ * @NApiVersion 2.0
+ * @NScriptType ClientScript   
+ * @NModuleScope SameAccount or Public
+ */
+
+define([],
+    function(){
+        function pageInit() {
+
+        }
+        function lineInit() {
+
+        }
+        function fieldChanged() {
+
+        }
+        function sublistChanged() {
+
+        }
+        function postSourcing() {
+
+        }
+        function validateDelete() {
+
+        }
+        function validateField() {
+
+        }
+        function validateInsert() {
+
+        }
+        function validateLine() {
+
+        }
+        function localizationContextEnter() {
+
+        }
+        function localizationContextExit() {
+
+        }
+
+        return {
+            pageInit: pageInit,
+            lineInit: lineInit,
+            fieldChanged: fieldChanged,
+            sublistChanged: sublistChanged,
+            postSourcing: postSourcing,
+            validateDelete: validateDelete,
+            validateField: validateField,
+            validateInsert: validateInsert,
+            validateLine: validateLine,
+            localizationContextEnter: localizationContextEnter,
+            localizationContextExit: localizationContextExit
+        }
+
+ });
---
+++ ue_template.js
@@ -0,0 +1,22 @@
+/**
+ * @NApiVersion 2.0
+ * @NScriptType UserEventScript   
+ */
+
+ define([],
+    function(){
+        function beforeLoad() {
+
+        }
+        function beforeSubmit(){
+
+        }
+        function afterSubmit() {
+
+        }
+        return {
+            beforeLoad: beforeLoad,
+            beforeSubmit: beforeSubmit,
+            afterSubmit: afterSubmit
+        }
+ });
